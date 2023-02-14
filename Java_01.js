e1cfa670000580f79cb820f3208a60e8bdd2c4fc
ignashevaaa <113353325+ignashevaaa@users.noreply.github.com>
2023-02-14 11:37:58 +0300

    Add files via upload

--- Java_01.js
+++ Java_01.js
@@ -10,3 +10,12 @@ var greeting = "Привет, ";
 var myName = "Ник";
 greeting + myName;
 myName.length;
+
+var name_orz="ОРЗ", name_consumption= "Чахотка"; 
+num_orz_mon=5;
+num_consumption_mon=3;
+num_consumption_tues=2;
+num_orz_tues=5;
+num_orz_week=num_orz_mon + num_orz_tues;
+num_consumption_week=num_consumption_mon + num_consumption_tues;
+name_orz+num_orz_week + name_consumption + num_consumption_week;
